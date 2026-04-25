2bec1ee87b0ecbee69ad74f95e843b831c3a861c
Caleb Magoola <95992141+CalebMagoola@users.noreply.github.com>
2026-04-25 19:06:28 -0400

    Attempt At Making Test File
    Doesn't work yet, returns null Attribute Error but I need to go

--- GriddedClimatologyToolTest.py
+++ GriddedClimatologyToolTest.py
@@ -1,1 +1,17 @@
-import pytest
+import sys
+import os
+import importlib.util
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+pyt_path = os.path.join(BASE_DIR, "GriddedClimatologyTool.pyt")
+
+spec = importlib.util.spec_from_file_location("toolbox", pyt_path)
+toolbox = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(toolbox)
+
+Tool = toolbox.Tool
+
+def test_import_works():
+    assert Tool is not None
+
+    
